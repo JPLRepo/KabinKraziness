ac62464b793b33e34a03c02ccd9c1bc9cd9c949a
Jamie Leighton <jamieleighton@bigpond.com>
2015-06-23 20:33:06 +1000

    V0.3.1.0
    KSP 1.0.3 support

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.*")]
-[assembly: KSPAssembly("KabinKraziness", 3, 0)]
-[assembly: KSPAssemblyDependency("KKInterfaces", 2, 0)]
-[assembly: KSPAssemblyDependency("AYInterfaces", 2, 0)]
+[assembly: AssemblyVersion("0.3.1.0")]
+[assembly: KSPAssembly("KabinKraziness", 3, 1)]
+[assembly: KSPAssemblyDependency("KKInterfaces", 2, 1)]
+[assembly: KSPAssemblyDependency("AYInterfaces", 2, 1)]
